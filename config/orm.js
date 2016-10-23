d54f56c39e91dcbab4b281a9dd490bff8375af2f
Shaun Fuller <fullers@email.unc.edu>
2016-10-23 12:57:31 -0400

    update code

--- config/orm.js
+++ config/orm.js
@@ -1,22 +1,74 @@
-var orm = require('../config/orm.js');
+/*
+Here is the O.R.M. where you write functions that takes inputs and conditions and turn them into database commands like SQL.
+*/
+var connection = require('../config/connect.js');
 
-var burger = {
-	selectAll: function (cb) {
-		orm.all('burger', function (res) {
-			cb(res);
+function printQuestionMarks(num) {
+	var arr = [];
+
+	for (var i = 0; i < num; i++) {
+		arr.push('?');
+	}
+
+	return arr.toString();
+}
+
+function objToSql(ob) {
+	// column1=value, column2=value2,...
+	var arr = [];
+
+	for (var key in ob) {
+		if (ob.hasOwnProperty(key)) {
+			arr.push(key + '=' + ob[key]);
+		}
+	}
+
+	return arr.toString();
+}
+
+var orm = {
+	selectAll: function (tableInput, cb) {
+		var queryString = 'SELECT * FROM ' + tableInput + ';';
+		connection.query(queryString, function (err, result) {
+			if (err) throw err;
+			cb(result);
 		});
 	},
-	// cols and vals are arrays
-	insertOne: function (cols, vals, cb) {
-		orm.create('burger', cols, vals, function (res) {
-			cb(res);
+		// vals is an array of values that we want to save to cols
+		// cols are the columns we want to insert the values into
+	insertOne: function (table, cols, vals, cb) {
+		var queryString = 'INSERT INTO ' + table;
+
+		queryString = queryString + ' (';
+		queryString = queryString + cols.toString();
+		queryString = queryString + ') ';
+		queryString = queryString + 'VALUES (';
+		queryString = queryString + printQuestionMarks(vals.length);
+		queryString = queryString + ') ';
+
+		console.log(queryString);
+
+		connection.query(queryString, vals, function (err, result) {
+			if (err) throw err;
+			cb(result);
 		});
 	},
-	updateOne: function (objColVals, condition, cb) {
-		orm.update('burger', objColVals, condition, function (res) {
-			cb(res);
+		// objColVals would be the columns and values that you want to update
+		// an example of objColVals would be {name: panther, sleepy: true}
+	updateOne: function (table, objColVals, condition, cb) {
+		var queryString = 'UPDATE ' + table;
+
+		queryString = queryString + ' SET ';
+		queryString = queryString + objToSql(objColVals);
+		queryString = queryString + ' WHERE ';
+		queryString = queryString + condition;
+
+		console.log(queryString);
+		connection.query(queryString, function (err, result) {
+			if (err) throw err;
+			cb(result);
 		});
 	}
 };
 
-module.exports = burger;
+module.exports = orm;
