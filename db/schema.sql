5dc725dc24c61f2a9fc6199167a7bc5f48d95a01
Shaun Fuller <fullers@email.unc.edu>
2016-10-23 21:46:10 -0400

    updated schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,9 +4,9 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
-	devoured boolean default false,
-	`date` timestamp,
+	id INT AUTO_INCREMENTNOT NOT NULL,
+	burger_name VARCHAR(255) NOT NULL,
+	devoured BOOLEAN default false,
+	`date` TIMESTAMP,
 	PRIMARY KEY (id)
 );
