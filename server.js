c88d5196eec25c2d679e625520f8696a6f74513b
Shaun Fuller <fullers@email.unc.edu>
2016-10-25 17:30:29 -0400

    Add process.env.PORT to server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.set('view engine', 'handlebars');
 var routes = require('./controllers/burgers_controller.js');
 app.use('/', routes);
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Starts the server to begin listening
 // =============================================================
